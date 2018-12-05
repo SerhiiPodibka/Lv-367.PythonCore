982ec3921fe0929e20d73207db58250a6ad4cb30
Serhii Podibka <podibkas@gmail.com>
2018-12-05 20:28:27 +0200

    add

---
+++ classwork3.py
@@ -0,0 +1,35 @@
+nums = [] #створюємо порожній список
+k=int(input("введіть кількість елементів")) #вводимо кількість елементів списку
+for i in range(k):
+    n=int(input("enter the element"))
+    nums.append(n) #обєднуємо елементи списку
+print (nums)
+max = nums[0]
+min = nums[0]
+for i in range(k):
+    if nums[i]>max:
+        max=nums[i]
+
+    if nums[i]<min:
+        min=nums[i]
+print ("max number = %d.min number = %d."%(max,min))
+
+for x in range (1,11):
+    if x%2 ==0:
+        print (x,"is even na 2")
+    elif x%3 ==0:
+        print (x,"is even na 3")
+    else:
+        print (x,"is na 2 and na 3")
+
+
+while True:
+    a = int(input('chuslo'))
+    if a<=0:
+        break
+
+k=int(input("введіть кількість елементів"))
+for i in range(k):
+    n=int(input("enter the element"))
+    if n<=0:
+        break
---
+++ sheeps.py
@@ -0,0 +1,7 @@
+def count_sheeps(arrayOfSheeps):
+  sheep=0
+  for i in arrayOfSheeps:
+      if i==True:
+          sheep=sheep+1
+  return sheep
+  print (sheep)
